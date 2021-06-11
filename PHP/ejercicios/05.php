053cae4c578992057abefbbe8876ee2c897fbfe0
gaby y edy <gabriela.lo180842@cbta35.edu.mx>
2021-06-11 13:42:45 -0500

    Subiendo archivo PHP 05

---
+++ PHP/ejercicios/05.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html >
+ <head>
+        <meta charset="UTF-8">
+         <title>Modelo de caja (box-sizing)</title>
+          </head>
+     <body>
+
+
+        <div class="contenedor">
+        	<h1>Aprendiendo PHP</h1>
+
+
+        	  <div class="contenido">
+        	  	<?php
+        	  	        $titulo = "Aprendiendo PHP";
+        	  	  
+        	  	?>
+        	  	<?php // imprime tu titulo ?>
+        	  	<?php echo $titulo; ?>
+
+        	  	
+        	  </div>
+        </div> 
+   </body>
+</html>	
