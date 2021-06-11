235e843155d7a42fda8cbbfaf73ce7822ff9a00f
gaby y edy <gabriela.lo180842@cbta35.edu.mx>
2021-06-11 15:19:49 -0500

    Subiendo archivo PHP 08

---
+++ PHP/ejercicios/08.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html >
+ <head>
+        <meta charset="UTF-8">
+         <title>Aprendiendo PHP</title>
+          </head>
+     <body>
+
+
+        <div class="contenedor">
+        	<h1>Aprendiendo PHP</h1>
+
+
+        	  <div class="contenido">
+        	  	<?php
+        	  	    $lenguaje = "MySQL";
+
+        	  	     switch ($lenguaje){
+        	  	        case 'PHP':
+        	  	             echo "Backend";
+        	  	             break;
+        	  	        case 'JavaScript':
+        	  	             echo "FrontEnd y Backend (NodeJS)";
+        	  	             brack;
+        	  	        case 'HTML5':
+        	  	             echo "FrontEnd";
+        	  	             Breack;
+        	  	        default;
+        	  	             echo "no valido";
+        	  	             Breack;
+        	        }
+        	     ?>
+
+            </div>
+       </div>
+   </body>
+ </html>
